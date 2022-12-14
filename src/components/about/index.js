774337be2819bc7acd17c9eff7c19ae8ebc33eb8
brycestampfl <brycestampfl@gmail.com>
2022-12-13 19:14:03 -0800

    added test file for cloudinary

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
-import  '../../styles.css'
+import '../../styles.css'
 import Garden from '../../images/Garden.jpg'
 
 const About = () => {
-    return(
+
+    results = "";
+    async function getResults() {
+        const results = await fetch(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image`, {
+            headers: {
+                Authorization: `Basic ${Buffer.from(process.env.CLOUDINARY_API_KEY + ':' + process.env.CLOUDINARY_API_SECRET).toString('base64')}`
+            }
+        }).then(r => console.log(r.json()));
+    }
+    getResults();
+
+    return (
         <div className='jumbotron'>
             <img src={Garden} />
         </div>
