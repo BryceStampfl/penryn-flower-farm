6386fb6c81d9a8c366cfb071d289c3e32adf5d83
brycestampfl <brycestampfl@gmail.com>
2022-12-13 20:26:22 -0800

    testing

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -4,6 +4,8 @@ import Garden from '../../images/Garden.jpg'
 import Buffer from 'buffer'
 
 const About = () => {
+    const [images, setImages] = React.useState([])
+
 
     const results = "";
     async function getResults() {
@@ -11,9 +13,11 @@ const About = () => {
             headers: {
                 Authorization: `Basic ${Buffer.from(process.env.REACT_APP_CLOUDINARY_API_KEY + ':' + process.env.REACT_APP_CLOUDINARY_API_SECRET).toString('base64')}`
             }
-        }).then(r => console.log(r.json()));
+        }).then(r => setImages(r.json()));
+
     }
     getResults();
+    console.log(images)
 
     return (
         <div className='jumbotron'>
