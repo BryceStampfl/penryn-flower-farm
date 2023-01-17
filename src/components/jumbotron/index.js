f5753137eb7589913e53452fb22a8641aaf8c23c
brycestampfl <brycestampfl@gmail.com>
2023-01-16 17:21:10 -0800

    updated jumbotron

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../../styles.css'
-import Garden from '../../images/shovel.jpg'
+import Garden from '../../images/Zinea.jpg'
 
 const Jumbotron = () => {
     return (
