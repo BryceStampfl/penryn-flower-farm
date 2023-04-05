234263054c009d2d85698a7500491bc5e99953d1
brycestampfl <brycestampfl@gmail.com>
2023-04-05 14:25:11 -0700

    added subscription and giftcard images for the flower product screen

--- src/components/flowers/index.js
+++ src/components/flowers/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { API, Storage } from 'aws-amplify';
 import { listProducts } from '../../graphql/queries';
-import { Loader, Collection, View, Flex, Text, Heading } from '@aws-amplify/ui-react';
+import { Loader, Collection, View, Flex, Text, Divider, Image } from '@aws-amplify/ui-react';
 import FlowerCard from './FlowerCard';
+
+
+import subscription from '../../images/basketSub.jpg'
+import giftcard from '../../images/giftcard.jpg'
+
 
 export const Flowers = () => {
     const [productData, setProductData] = React.useState(null);
@@ -25,30 +30,65 @@ export const Flowers = () => {
 
     if (productData === null) { return (<div><Loader size='large' /></div>) }
     return (
-        <View>
-            <Collection
-                margin='0 auto 0 auto'
-                items={productData}
-                type='grid'
-                isSearchable
-                searchPlaceholder="Type to search..."
-                searchNoResultsFound={
-                    <Flex justifyContent="center">
-                        <Text color="black" fontSize="1rem">
-                            Nothing found, please try again
-                        </Text>
-                    </Flex>
-                }
-                isPaginated
-                itemsPerPage={6}
-                templateRows='1fr'
-                templateColumns={{ base: '1fr', medium: '1fr 1fr 1fr ' }}
-            >
-                {(item, index) => (
+        <View
+        // border='1px solid gray'
+        >
+            <View  >
+                <Flex justifyContent='space-around' maxWidth={{ medium: '1540px' }}
+                // backgroundColor='#F7F7F7'
+                >
+                    <div style={{ width: 'auto', margin: '0 auto 0 auto' }}>
+                        <Image src={subscription} />
+                    </div>
 
-                    <FlowerCard key={index} data={item} />
-                )}
-            </Collection>
+                    <div style={{ width: 'auto', margin: '0 auto 0 auto' }}>
+                        <Image src={giftcard} />
+                    </div>
+                </Flex>
+            </View>
+
+
+
+            <View>
+
+                <View
+                    margin='0.5rem 2rem 0 2rem'
+
+                >
+                    <Divider
+                        size="small"
+                        orientation="horizontal" />
+                </View>
+
+                <Text
+                    fontFamily='Cormorant'
+                    fontSize='2.5rem'
+                >Browse Selection</Text>
+
+                <Collection
+                    items={productData}
+                    type='grid'
+                    isSearchable
+                    searchPlaceholder="Type to search..."
+                    searchNoResultsFound={
+                        <Flex justifyContent="center">
+                            <Text color="black" fontSize="1rem">
+                                Nothing found, please try again
+                            </Text>
+                        </Flex>
+                    }
+                    isPaginated
+                    itemsPerPage={6}
+                    templateRows='1fr'
+                    templateColumns={{ base: '1fr', medium: '1fr 1fr 1fr ' }}
+                >
+                    {(item, index) => (
+
+                        <FlowerCard key={index} data={item} />
+                    )}
+                </Collection>
+            </View>
+
         </View >
     )
 }
